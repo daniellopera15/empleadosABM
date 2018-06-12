8a28dd39f349bc61a32e9dae9052dc818a36dc83
Daniel Lopera <dlopera@tecnosoftware.com>
2018-06-12 12:26:37 -0300

    Se modifico el cross origin en la clase Controller

--- empleados/src/main/java/com/tecnosoftware/empleados/controller/EmpleadosController.java
+++ empleados/src/main/java/com/tecnosoftware/empleados/controller/EmpleadosController.java
@@ -19,6 +19,7 @@ import java.util.Map;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/empleado")
 public class EmpleadosController {
 
@@ -28,13 +29,11 @@ public class EmpleadosController {
     private EmpleadoService empleadoService;
 
     @RequestMapping(value = "/crear", method = RequestMethod.POST)
-    @CrossOrigin
     public void crearEmpleado(@Valid @RequestBody Empleado empleado) {
         empleadoService.crearEmpleado(empleado);
     }
 
     @RequestMapping(value = "/editar", method = RequestMethod.PUT)
-    @CrossOrigin
     public ResponseEntity<Empleado> editarTodo(@Valid @RequestBody Empleado empleadomodificado) throws EmpleadoNotFoundException {
         if(empleadoService.editarTodo(empleadomodificado.getId(), empleadomodificado)){
             return ResponseEntity.ok(empleadomodificado);
@@ -44,9 +43,7 @@ public class EmpleadosController {
 
     }
 
-
     @RequestMapping(value = "/eliminar/{empleadoId}", method = RequestMethod.DELETE)
-    @CrossOrigin
     public ResponseEntity<Empleado> eliminarEmpleado(@PathVariable Integer empleadoId) throws EmpleadoNotFoundException {
         Empleado empleado=empleadoService.buscarEmpleado(empleadoId);
 
@@ -61,19 +58,16 @@ public class EmpleadosController {
 
 
     @RequestMapping("/list")
-    @CrossOrigin
     public List<Empleado> list() {
         return empleadoService.list();
     }
 
     @RequestMapping("/buscar/edad/{edad}")
-    @CrossOrigin
     public List<Empleado> list(@PathVariable int edad) {
         return empleadoService.buscarPorEdad(edad);
     }
 
     @RequestMapping(value = "/buscar/{empleadoId}")
-    @CrossOrigin
     public ResponseEntity<Empleado> buscarEmpleado(@PathVariable Integer empleadoId) throws EmpleadoNotFoundException {
         Empleado empleado = empleadoService.buscarEmpleado(empleadoId);
         if (empleado == null) {
